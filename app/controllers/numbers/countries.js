b725553b0f603e4409d5489813e567b6f811a536
domingo <ing.domingo.sl@gmail.com>
2023-09-20 14:23:31 +0200

    Added flag support

--- app/controllers/numbers/countries.js
+++ app/controllers/numbers/countries.js
@@ -4,7 +4,16 @@ const Numbers = mongoose.model('Number');
 
 export default async ({resolve}) => {
 
-    const countries = await Numbers.distinct("location.country");
+    const countries = await Numbers.aggregate([
+        {
+            '$group': {
+                '_id': '$location.country',
+                'flag': {
+                    '$first': '$location.flag'
+                }
+            }
+        }
+    ]);
 
     resolve(countries);
 
